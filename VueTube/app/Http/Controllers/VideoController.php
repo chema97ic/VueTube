69d82281c6c4c9d4d523a1c1f9b783dd4572c82e
DESKTOP-OURHU63\chema <chematerrax@hotmail.es>
2019-10-13 22:05:36 +0200

    Añadida vista de videos
    Añadida la vista de los videos y el reproductor

--- VueTube/app/Http/Controllers/VideoController.php
+++ VueTube/app/Http/Controllers/VideoController.php
@@ -8,6 +8,10 @@ use Vuetube\Video;
 class VideoController extends Controller
 {
     public function show(Video $video) {
-        return $video;
+        if(request()->wantsJson()) {
+            return $video;
+        }
+
+        return view('video', compact('video'));
     }
 }
--- VueTube/resources/views/layouts/app.blade.php
+++ VueTube/resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('styles')
 </head>
 <body>
     <div id="app">
@@ -95,5 +96,8 @@
             
         }
     </script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')
+
 </body>
 </html>

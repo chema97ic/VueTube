ef9af1087d674988b0307cd3c2639f198a6368ed
DESKTOP-OURHU63\chema <chematerrax@hotmail.es>
2019-12-26 19:54:02 +0100

    login css
    añadidos estilos al login

--- VueTube/resources/views/auth/login.blade.php
+++ VueTube/resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container h-100 ">
+<!--<div class="container h-100 ">
     <div class="row justify-content-center">
         <div class="col-md-8 vertical-center">
             <div class="">
@@ -55,16 +55,61 @@
                                 <button type="submit" class="btn btn-primary">
                                     Identificarse
                                 </button>
-
-                                <!--@if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif-->
                             </div>
                         </div>
                     </form>
                 </div>
+            </div>
+        </div>
+    </div>
+</div>-->
+<div class="d-flex justify-content-center h-100 w-100 vertical-center">
+    <div class="user_card">
+        <div class="d-flex justify-content-center">
+            <div class="brand_logo_container">
+                <img src="https://static.zerotackle.com/images/team-flags/st-george-illawarra-dragons-round.png" class="brand_logo" alt="Logo">
+            </div>
+        </div>
+        <div class="d-flex justify-content-center form_container">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="input-group mb-3">
+                    <div class="input-group-append">
+                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                    </div>
+                    <input type="email" id="email" name="email" class="form-control input_user" placeholder="Email" required value="{{ old('email') }}" required autofocus>
+                    @if ($errors->has('email'))
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="input-group mb-2">
+                    <div class="input-group-append">
+                        <span class="input-group-text"><i class="fas fa-key"></i></span>
+                    </div>
+                    <input type="password" name="password" id="password" class="form-control input_pass" placeholder="Contraseña" required>
+                    @if ($errors->has('password'))
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="customControlInline">
+                        <label class="custom-control-label" for="customControlInline">Recordar contraseña</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center mt-3 login_container">
+                    <button type="submit" class="btn login_btn">Entrar</button>
+                </div>
+            </form>
+        </div>
+
+        <div class="mt-4">
+            <div class="d-flex justify-content-center links">
+                ¿Aún no estás registrado? <a href="#" class="ml-2">Registraté!</a>
             </div>
         </div>
     </div>
@@ -73,13 +118,81 @@
 
 @section('styles')
 <style>
-    .vertical-center {
-        margin: 0;
-        position: absolute;
-        top: 50%;
-        -ms-transform: translateY(-50%);
-        transform: translateY(-50%);
-    }
+        .vertical-center {
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            -ms-transform: translateY(-50%);
+            transform: translateY(-50%);
+        }
+    	body,
+		html {
+			margin: 0;
+			padding: 0;
+			height: 100%;
+			background: #60a3bc !important;
+		}
+		.user_card {
+			height: 400px;
+			width: 350px;
+			margin-top: auto;
+			margin-bottom: auto;
+			background: #f39c12;
+			position: relative;
+			display: flex;
+			justify-content: center;
+			flex-direction: column;
+			padding: 10px;
+			box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+			-webkit-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+			-moz-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+			border-radius: 5px;
+		}
+		.brand_logo_container {
+			position: absolute;
+			height: 170px;
+			width: 170px;
+			top: -75px;
+			border-radius: 50%;
+			background: #60a3bc;
+			padding: 10px;
+			text-align: center;
+		}
+		.brand_logo {
+			height: 150px;
+			width: 150px;
+			border-radius: 50%;
+			border: 2px solid white;
+		}
+		.form_container {
+			margin-top: 100px;
+		}
+		.login_btn {
+			width: 100%;
+			background: #c0392b !important;
+			color: white !important;
+		}
+		.login_btn:focus {
+			box-shadow: none !important;
+			outline: 0px !important;
+		}
+		.login_container {
+			padding: 0 2rem;
+		}
+		.input-group-text {
+			background: #c0392b !important;
+			color: white !important;
+			border: 0 !important;
+			border-radius: 0.25rem 0 0 0.25rem !important;
+		}
+		.input_user,
+		.input_pass:focus {
+			box-shadow: none !important;
+			outline: 0px !important;
+		}
+		.custom-checkbox .custom-control-input:checked~.custom-control-label::before {
+			background-color: #c0392b !important;
+		}
 </style>
 @endsection
 
